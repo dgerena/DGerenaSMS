294dc41093620229f9c79e02e9145c7c997e3175
Eli <temp@temps-MacBook-Pro.local>
2013-11-05 19:34:58 -0500

    trying to css… not doing to hot.
    cant seem to get my buttons to form right.

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,1 +1,12 @@
 
+var flashReady= function()
+{
+	//enter all your video functions and workings in here SMARTY
+}
+var flashReady=function()
+{
+$('button').on('click',function(e)
+{
+	connect('rtmp://localhost/SMSServer')
+})
+}
