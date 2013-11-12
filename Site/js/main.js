5dd22a46b45c549ebd4dc9c0191c13e1fc75f6df
Eli <temp@temps-MacBook-Pro.local>
2013-11-12 14:01:29 -0500

    a few things
    Cleaned up some links and script tags. also adressed some css issues
    with volume and timeline bar.
    still in air on decisions to use a font or not for button iconography.

--- Site/js/main.js
+++ Site/js/main.js
@@ -2,11 +2,33 @@
 var flashReady= function()
 {
 	//enter all your video functions and workings in here SMARTY
+	var playBtn = document.getElementById("#play").onclick();
+	var stopBtn = document.getElementById("#stop").onclick();
+	var pauseBtn = document.getElementById("#pause").onclick();
+	var recordBtn = document.getElementById("#record").onclick();
+	var volumeBtn = document.getElementById("#volume").onkeydown();
+	playBtn.startPlayback();
+	document.getElementById('play').onclick = function() {
+  alert('click!')
+}
+
+/*
+	function play(Event, click){
+		startPlayback();
+	};
+*/
+
 }
 var flashReady=function()
 {
-$('button').on('click',function(e)
-{
-	connect('rtmp://localhost/SMSServer')
-})
+	$('button').on('click',function(e)
+	{
+		connect('rtmp://localhost/SMSServer')
+	})	
 }
+//get mic code
+//getMicrophones()
+//@return microphones -(Array) a list of all available cameras
+// video 
+//getCameras()
+//@return cameras -(array) a list of all the available cameras
