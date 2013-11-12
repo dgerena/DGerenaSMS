39d4ffae8fc9111c43d96ffe78679f7554a644da
Eli <temp@temps-MacBook-Pro.local>
2013-11-12 18:46:29 -0500

    fixed issues with buttons
    had help with all of the above.

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,18 +1,58 @@
 var flashReady=function()
 {
-	//enter all your video functions and workings in here SMARTY
-	var playBtn = document.getElementById("play");
-	var stopBtn = document.getElementById("#stop").onclick();
-	var pauseBtn = document.getElementById("#pause").onclick();
-	var recordBtn = document.getElementById("#record").onclick();
-	var volumeBtn = document.getElementById("#volume").onkeydown();
-	playBtn.onclick(alert('click!'));
+	var dom={
 	
-	$('#play').on('click',function(e)
+	//enter all your video functions and workings in here SMARTY
+	playBtn:document.getElementById("play"),
+	stopBtn:document.getElementById("stop"),
+	pauseBtn:document.getElementById("pause"),
+	recordBtn:document.getElementById("record"),
+	volumeBtn:document.getElementById("volume"),
+	micOpt:document.getElementById("microphone"),
+	camOpt:document.getElementById("camera")
+	}
+	playedOut();
+	function playedOut(){
+		$(dom.playBtn).click(function(e)// play button
+		{
+			e.preventDefault();
+			flash.connect('rtmp://localhost/SMSServer');
+			$(this).unbind();
+		});
+	};
+	$(dom.stopBtn).click(function(e)
 	{
-		connect('rtmp://localhost/SMSServer')
-	})	
-}
+		e.preventDefault();
+		flash.stopPlaying();
+		playedOut();
+	});
+	$(dom.pauseBtn).click(function(e){
+		e.preventDefault();
+		flash.playPause();
+		playedOut();
+	});
+	$(dom.camOpt).click(function(e){
+		flash.getCameras();
+	});
+	$(dom.micOpt).click(function(e){
+		console.log("herro");
+		var micArr=flash.getMicrophones();
+		console.log(micArr.length);
+		for(var i=0; i<micArr.length;i++){
+			$(dom.micOpt).append('<option>'+micArr[i]+'</option>');
+			console.log("shit");
+		}
+		$(this).unbind();
+	});
+	
+};
+function connected(success,error){
+		if(success){
+			flash.startPlaying("hobbit_vp6.flv");
+			
+		}
+		console.log(error)
+};
 //get mic code
 //getMicrophones()
 //@return microphones -(Array) a list of all available cameras
